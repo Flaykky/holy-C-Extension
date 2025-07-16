fe82abc2d3da5257678b404e4a2c45668fcacf82
flaykDev <138144836+Flaykky@users.noreply.github.com>
2025-07-16 22:41:42 +0300

    Add files via upload

---
+++ src/extension.ts
@@ -0,0 +1,8 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  // Расширение активируется при открытии файлов holyc,
+  // но дополнительных команд пока нет.
+}
+
+export function deactivate() {}
